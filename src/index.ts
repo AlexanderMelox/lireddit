5dc383397b5f3a7bc8bb2d1b26409f5acfd89807
Alexander Melo <alexandermelox@gmail.com>
2020-08-25 09:17:20 -0500

    Set up MikroORM

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello");
+import { MikroORM } from '@mikro-orm/core'
+import { __prod__ } from './constants'
+import { Post } from './entities/Post'
+import mircroConfig from './mikro-orm.config'
+
+const main = async () => {
+  const orm = await MikroORM.init(mircroConfig)
+  const { em } = orm
+  orm.getMigrator().up()
+
+  // const post = em.create(Post, { title: 'my first post' })
+  // await em.persistAndFlush(post)
+
+  const posts = await em.find(Post, {})
+  console.log(posts)
+}
+
+main().catch((err) => {
+  console.error(err)
+})
