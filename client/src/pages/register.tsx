ef4e8909e85809c796afe46daee499cadd63a2e0
Alexander Melo <alexandermelox@gmail.com>
2020-09-02 12:32:36 -0500

    Added client with Next.js and Register page

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Formik, Form } from 'formik'
+import { Wrapper } from '../components/Wrapper'
+import { InputField } from '../components/InputField'
+import { Box, Button } from '@chakra-ui/core'
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ values, handleChange, isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              label="Username"
+              placeholder="username"
+            />
+            <Box mt={4}>
+              <InputField
+                type="password"
+                name="password"
+                label="Password"
+                placeholder="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              isLoading={isSubmitting}
+              type="submit"
+              variantColor="teal"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  )
+}
+
+export default Register
